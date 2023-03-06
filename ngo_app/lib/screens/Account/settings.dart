2ac809da3e782af01c5f49592ca1a5a3729c6d80
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-03-07 01:09:51 +0530

    made settings page

--- ngo_app/lib/screens/Account/settings.dart
+++ ngo_app/lib/screens/Account/settings.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:sliding_switch/sliding_switch.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class settings_user extends StatefulWidget {
   const settings_user({super.key});
@@ -21,13 +23,66 @@ class _settings_userState extends State<settings_user> {
             leading: BackButton(
               onPressed: () => Navigator.of(context).pop(false),
             ),
-            // leading: IconButton(
-            //   icon: const Icon(Icons.arrow_back),
-
-            //   onPressed: () {},
-            // ),
             backgroundColor: Colors.blue,
-            title: const Text("Settings"), elevation: 0.0,
+            title: const Text("Settings"),
+            elevation: 0.0,
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 5, left: 10, right: 10),
+              child: Container(
+                height: 100,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5, left: 10),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: 'Notifications\n',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Roboto-Black',
+                            color: Colors.black,
+                          ),
+                          children: const <TextSpan>[
+                            TextSpan(
+                              text:
+                                  "Turning off will disable all the\n notifications from Ngo's and and App",
+                              style: TextStyle(
+                                  fontSize: 14.0,
+                                  fontFamily: 'Roboto-Black',
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 60,
+                      ),
+                      SlidingSwitch(
+                        value: true,
+                        width: 55,
+                        height: 25,
+                        textOff: "OFF",
+                        textOn: "ON",
+                        contentSize: 12,
+                        animationDuration: const Duration(milliseconds: 300),
+                        onChanged: (bool value) {},
+                        //disable these
+                        onTap: () {},
+                        onDoubleTap: () {},
+                        onSwipe: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       );
