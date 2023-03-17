0358b2902c30c47b996752d61be2a7b11464e5bb
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-03-17 16:10:45 +0530

    made manage address page

--- ngo_app/lib/screens/Account/manage_address.dart
+++ ngo_app/lib/screens/Account/manage_address.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:form_builder_phone_field/form_builder_phone_field.dart';
 
 class manage_address extends StatefulWidget {
   const manage_address({super.key});
@@ -21,7 +24,142 @@ class _manage_addressState extends State<manage_address> {
             title: const Text("Manage Address"),
             elevation: 0.0,
           ),
-          
+          body: SafeArea(
+            child: SingleChildScrollView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(child: Container(), flex: 0),
+                  FormBuilder(
+                    // key: _formKey,
+                    child: Column(children: <Widget>[
+                      FormBuilderTextField(
+                        name: "name",
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          border: OutlineInputBorder(
+                            gapPadding: 10.0,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      FormBuilderTextField(
+                        name: "name",
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          suffixIcon: Icon(
+                            Icons.location_on,
+                          ),
+                          labelText: 'Address',
+                          border: OutlineInputBorder(
+                            gapPadding: 10.0,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      FormBuilderTextField(
+                        name: "name",
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Postal/ZIP Code',
+                          border: OutlineInputBorder(
+                            gapPadding: 10.0,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      FormBuilderTextField(
+                        name: "name",
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Town/City',
+                          border: OutlineInputBorder(
+                            gapPadding: 10.0,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      FormBuilderTextField(
+                        name: "name",
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'State',
+                          border: OutlineInputBorder(
+                            gapPadding: 10.0,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      FormBuilderTextField(
+                        name: "name",
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Country',
+                          border: OutlineInputBorder(
+                            gapPadding: 10.0,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {},
+                        child: const Text(
+                          "SAVE DETAILS",
+                          style: TextStyle(
+                              letterSpacing: 0.3,
+                              fontSize: 17,
+                              fontWeight: FontWeight.w200,
+                              color: Colors.white),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          elevation: 0.0,
+                          backgroundColor: Colors.blue,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 120, vertical: 10),
+                        ),
+                      ),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       );
 }
