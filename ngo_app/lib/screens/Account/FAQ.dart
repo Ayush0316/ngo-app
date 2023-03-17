89d5099901ffd278abf3f149caea89a42a6fa64c
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-03-17 17:09:57 +0530

    added faq's page

--- ngo_app/lib/screens/Account/FAQ.dart
+++ ngo_app/lib/screens/Account/FAQ.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:getwidget/getwidget.dart";
 
 class faq extends StatefulWidget {
   const faq({super.key});
@@ -12,7 +13,7 @@ class _faqState extends State<faq> {
   Widget build(BuildContext context) => GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
         child: Scaffold(
-          backgroundColor: Color.fromARGB(249, 255, 255, 255),
+          backgroundColor: Colors.blue[50],
           appBar: AppBar(
             leading: BackButton(
               onPressed: () => Navigator.of(context).pop(false),
@@ -24,6 +25,41 @@ class _faqState extends State<faq> {
           body: SafeArea(
             child: SingleChildScrollView(
               keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+              child: Column(children: <Widget>[
+                GFAccordion(
+                  // collapsedIcon: Icon(Icons.add),
+                  // expandedIcon: Icon(Icons.minimize),4
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    // fontWeight: FontWeight.normal,
+                    fontFamily: 'Roboto-Black',
+                  ),
+                  titleBorderRadius: const BorderRadius.all(Radius.circular(8)),
+                  contentBorderRadius:
+                      const BorderRadius.all(Radius.circular(8)),
+                  title: "Lorem Ipsum is simply dummy",
+                  content:
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                ),
+                GFAccordion(
+                  // collapsedIcon: Icon(Icons.add),
+                  // expandedIcon: Icon(Icons.minimize),4
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    // fontWeight: FontWeight.normal,
+                    fontFamily: 'Roboto-Black',
+                  ),
+                  titleBorderRadius: const BorderRadius.all(Radius.circular(8)),
+                  contentBorderRadius:
+                      const BorderRadius.all(Radius.circular(8)),
+                  title: "Lorem Ipsum is simply dummy",
+                  content:
+                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                ),
+              ]),
             ),
           ),
         ),
