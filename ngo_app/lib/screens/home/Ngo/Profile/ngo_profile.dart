af6e08aad2589a0a5a4a11521116ed8a8e5d23f2
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-03-19 15:33:21 +0530

    profile about section

--- ngo_app/lib/screens/home/Ngo/Profile/ngo_profile.dart
+++ ngo_app/lib/screens/home/Ngo/Profile/ngo_profile.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
 import "package:ngo_app/services/auth.dart";
 import 'package:ngo_app/services/UserProvider.dart';
+import 'package:ngo_app/responsive.dart';
 
 class profile extends StatefulWidget {
   const profile({super.key});
@@ -18,6 +19,8 @@ class _profileState extends State<profile> {
     final data = Provider.of<Data>(context).data;
     String name = data["name"] != null ? data["name"] : "Name";
     String url = data["Imgurl"] != null ? data["Imgurl"] : " ";
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
         child: Scaffold(
@@ -85,6 +88,37 @@ class _profileState extends State<profile> {
                         style: TextStyle(
                           fontWeight: FontWeight.normal,
                           fontFamily: 'Roboto-Black',
+                        ),
+                      ),
+                      Container(
+                        constraints: BoxConstraints(
+                          minHeight: 100,
+                          maxHeight: Responsive.isSmallScreen(context)
+                              ? height / 3
+                              : height,
+                          maxWidth:
+                              Responsive.isSmallScreen(context) ? width : width,
+                        ), //BoxConstraints
+
+                        margin: const EdgeInsets.all(20.0),
+                        padding: const EdgeInsets.only(
+                            left: 15, right: 15, top: 15, bottom: 15),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          ),
+                        ),
+
+                        child: Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incilaborumdidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis"
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incilaborumdidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Roboto-Black',
+                          ),
                         ),
                       ),
                     ],
