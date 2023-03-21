84b893c02d035a590aec2592a09afc35e0780bd2
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-03-21 10:44:06 +0530

    added pages for community profiles and lists

---
+++ ngo_app/lib/screens/Community/community_screens.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+class community_profile extends StatelessWidget {
+  const community_profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
